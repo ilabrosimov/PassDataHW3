728dcbe8ebd9027fc02de14eaef4fb7e060793a6
ilabrosimov <ilabrosimov@gmail.com>
2021-05-25 20:04:37 +0300

    Initial Commit

---
+++ PassDataHW3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassDataHW3
+//
+//  Created by ilabrosimov on 25.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

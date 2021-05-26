5cfb750ebee9d5e0f6f76607cc5840f2d7fefcd0
ilabrosimov <ilabrosimov@gmail.com>
2021-05-26 12:30:10 +0300

    Создана логика, переходы для приложения Добавлен ImageView во второй экран, изменены constraints

--- PassDataHW3/LoginViewController.swift
+++ PassDataHW3/LoginViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var forgotLoginButton: UIButton!
+    @IBOutlet weak var fotgotPasswordButton: UIButton!
+    
+//    override func viewWillAppear(_ animated: Bool) {
+//        loginTextField.text = ""
+//        passwordTextField.text = ""
+//    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+      
+        guard let destinationVC = segue.destination as? WelcomeViewController else {
+            return
+        }
+        destinationVC.loginName = loginTextField.text
+    }
+    @IBAction func unwind ( _segue: UIStoryboardSegue) {
+        loginTextField.text = ""
+        passwordTextField.text = ""
+    }
+    @IBAction func LogInTapped (_sender: UIButton!) {
+        guard loginTextField.text == "Login",
+              passwordTextField.text == "Password" else {
+            return
+        }
+        performSegue(withIdentifier: "goToWelcomeVC", sender: nil)
+    }
+   
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 
-
 }
-
--- PassDataHW3/WelcomeViewController.swift
+++ PassDataHW3/WelcomeViewController.swift
@@ -7,9 +7,19 @@
 
 import UIKit
 class WelcomeViewController : UIViewController {
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    var loginName: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        backgroundImageView.image =  UIImage(named: "WelcomeBackground")
+      welcomeLabel.text = "Welcome, \(loginName ?? "")!"
+    }
+    
+    @IBAction func logOutTapped (_sender: UIButton!) {
+       // dismiss(animated: true)
         
     }
 }

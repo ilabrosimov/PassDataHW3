14fb4db696bcc9f60aa5fb402794c307824e5a1c
ilabrosimov <ilabrosimov@gmail.com>
2021-05-26 17:17:24 +0300

    Добавлены alertControllers при неправильном вводе логина и пароля

--- PassDataHW3/LoginViewController.swift
+++ PassDataHW3/LoginViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+   //MARK: - IB Outlets
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
@@ -15,32 +17,52 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var forgotLoginButton: UIButton!
     @IBOutlet weak var fotgotPasswordButton: UIButton!
     
-//    override func viewWillAppear(_ animated: Bool) {
-//        loginTextField.text = ""
-//        passwordTextField.text = ""
-//    }
+    //MARK: - Private Propereties
+    private let loginName = "Ilia"
+    private let password = "Password"
     
+    //MARK: - Ovveride Methods
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-      
-        guard let destinationVC = segue.destination as? WelcomeViewController else {
-            return
-        }
+        guard let destinationVC = segue.destination as? WelcomeViewController else {return}
         destinationVC.loginName = loginTextField.text
     }
+    
     @IBAction func unwind ( _segue: UIStoryboardSegue) {
         loginTextField.text = ""
         passwordTextField.text = ""
     }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    //MARK: - IB ACTIONS
     @IBAction func LogInTapped (_sender: UIButton!) {
-        guard loginTextField.text == "Login",
-              passwordTextField.text == "Password" else {
+        guard loginTextField.text == loginName,
+              passwordTextField.text == password else {
+            showBasicAlert(on: self, with: "Mistake!", message: "Check out your login or password!")
             return
         }
         performSegue(withIdentifier: "goToWelcomeVC", sender: nil)
     }
-   
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
+    
+    @IBAction func forgotLoginTapped (_sender: UIButton!) {
+        if  loginTextField.text != loginName {
+            showBasicAlert(on: self, with: "Oooops", message: "Check your Login")
+        }
     }
+    
+    @IBAction func forgotPasswordTapped (_sender: UIButton!) {
+        if  passwordTextField.text != password {
+            showBasicAlert(on: self, with: "Oooops", message: "Check your Password")
+        }
+    }
+
+    // MARK: - PUBLIC METHODS
+    func showBasicAlert (on vc : UIViewController, with title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(alertAction)
+        vc.present(alert, animated: true, completion: nil)
+        }
 
 }
--- PassDataHW3/WelcomeViewController.swift
+++ PassDataHW3/WelcomeViewController.swift
@@ -7,19 +7,18 @@
 
 import UIKit
 class WelcomeViewController : UIViewController {
+    
+    //MARK: - IB Outlets
     @IBOutlet weak var backgroundImageView: UIImageView!
     @IBOutlet weak var welcomeLabel: UILabel!
     
+    //MARK: - Public Properties
     var loginName: String?
     
+    //MARK:- LifeCycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         backgroundImageView.image =  UIImage(named: "WelcomeBackground")
       welcomeLabel.text = "Welcome, \(loginName ?? "")!"
-    }
-    
-    @IBAction func logOutTapped (_sender: UIButton!) {
-       // dismiss(animated: true)
-        
     }
 }
